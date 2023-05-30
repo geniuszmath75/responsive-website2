e99fcee2f4f5f7c060e273afb1f438915b24ea2c
geniuszmath75 <geniuszmath75@gmail.com>
2023-05-30 15:06:01 +0200

    Updating navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Navbar from './components/Navbar';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/HomePage/Home';
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/' />
+        <Route path='/' exact Component={Home}/>
       </Routes>
     </Router>
   );
