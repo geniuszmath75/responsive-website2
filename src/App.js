969a1e00bd267662972a8f343db5c1a33c617f2f
geniuszmath75 <geniuszmath75@gmail.com>
2023-05-31 14:09:22 +0200

    Creating another pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Navbar from './components/Navbar';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/HomePage/Home';
+import Services from './components/pages/Services/Services';
+import Products from './components/pages/Products/Products';
+import SignUp from './components/pages/SignUp/SignUp';
+import Footer from './components/pages/Footer/Footer';
 
 function App() {
   return (
@@ -10,7 +14,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' exact Component={Home}/>
+        <Route path='/services' Component={Services}/>
+        <Route path='/products' Component={Products}/>
+        <Route path='/sign-up' Component={SignUp}/>
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/pages/Services/Services.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HeroSection from "../../HeroSection";
+import { homeObjTwo } from "./Data";
+
+function Services() {
+  return (
+    <>
+      <HeroSection {...homeObjTwo} />
+    </>
+  );
+}
+
+export default Services;
